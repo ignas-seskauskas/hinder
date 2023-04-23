b89d47f9d9cf98d39d71418e7a2c8761c9f4c29f
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-04-23 19:14:38 +0300

    Additional navigation & css fixes

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -3,17 +3,18 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-const MenuBarView = () => {
+const NavBarView = () => {
   return (
     <Navbar fixed="top" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/home">Hinder</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/hobby">Hobby</Nav.Link>
+          <Nav.Link href="/">Sign out</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
   );
 };
 
-export default MenuBarView;
+export default NavBarView;
